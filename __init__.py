c9b564170cd37966a10d205835f5383dafa0d2b8
jlampel <jlampel@cgcookie.com>
2021-10-29 15:21:22 -0700

    Update target Blender version

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ bl_info = {
     "name": "Scattershot - Voronoi Image Texture Scattering",
     "author": "Jonathan Lampel",
     "version": (1, 3),
-    "blender": (2, 92, 0),
+    "blender": (2, 93, 0),
     "location": "Shader Editor > Node",
     "description": "Quickly distributes image textures around your model with several controls for randomization",
     "warning": "",
